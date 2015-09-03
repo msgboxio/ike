70acc871c03c19841b68edbd9e5f7c046c01dac2
Abhishek Kumar <bettiah@gmail.com>
2015-09-03 13:31:30 +0800

    seperated rekey func

---
+++ rekey.go
@@ -0,0 +1,94 @@
+package ike
+
+import (
+	"errors"
+
+	"msgbox.io/ike/state"
+	"msgbox.io/log"
+)
+
+// rekeying can be started by either end
+
+type ReKeySession struct {
+	Session
+
+	newIkeSpiI, newIkeSpiR Spi
+	newTkm                 *Tkm
+}
+
+//  SK {SA, Ni, KEi} - ike sa
+func (o *ReKeySession) SendIkeSaRekey() {
+	var err error
+	suite := NewCipherSuite(o.cfg.IkeTransforms)
+	if o.newTkm, err = NewTkmInitiator(suite, o.tkm.ids); err != nil {
+		log.Error(err)
+		o.cancel(err)
+		return
+	}
+	o.newIkeSpiI = MakeSpi()
+	o.cfg.ProposalIke.Spi = o.newIkeSpiI
+	init := makeIkeChildSa(childSaParams{
+		isInitiator:   o.tkm.isInitiator,
+		spiI:          o.IkeSpiI,
+		spiR:          o.IkeSpiR,
+		proposals:     []*SaProposal{o.cfg.ProposalIke},
+		nonce:         o.newTkm.Ni,
+		dhTransformId: o.newTkm.suite.dhGroup.DhTransformId,
+		dhPublic:      o.newTkm.DhPublic,
+	})
+	init.IkeHeader.MsgId = o.msgId
+	// use old tkm to encrypt & sign
+	o.initIb, err = EncodeTx(init, o.tkm, o.conn, o.conn.RemoteAddr(), true)
+	if err != nil {
+		log.Error(err)
+		o.cancel(err)
+	}
+	o.msgId++
+}
+
+//  SK {SA, Nr, KEr} - ike sa
+func (o *ReKeySession) HandleSaRekey(msg interface{}) {
+	m := msg.(*Message)
+	if err := o.handleEncryptedMessage(m); err != nil {
+		log.Error(err)
+		return
+	}
+	if m.IkeHeader.Flags != RESPONSE {
+		return // TODO handle this later
+	}
+	if tsi := m.Payloads.Get(PayloadTypeTSi); tsi != nil {
+		log.V(1).Info("received CREATE_CHILD_SA for child sa")
+		return // TODO
+	}
+	if !EnsurePayloads(m, InitPayloads) {
+		err := errors.New("essential payload is missing from Ike Sa rekey message")
+		log.Error(err)
+		return
+	}
+	// TODO - currently dont support different prfs from original
+	keR := m.Payloads.Get(PayloadTypeKE).(*KePayload)
+	if err := o.newTkm.DhGenerateKey(keR.KeyData); err != nil {
+		log.Error(err)
+		return
+	}
+	// set Nr
+	no := m.Payloads.Get(PayloadTypeNonce).(*NoncePayload)
+	o.newTkm.Nr = no.Nonce
+	// get new IKE spi
+	peerSpi, err := getPeerSpi(m, IKE)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	o.newIkeSpiR = append([]byte{}, peerSpi...)
+	// create rest of ike sa
+	o.newTkm.IsaCreate(o.newIkeSpiI, o.newIkeSpiR, o.tkm.skD)
+	log.Infof("NEW IKE SA Established: [%s]%#x<=>%#x[%s]",
+		o.conn.LocalAddr(),
+		o.newIkeSpiI,
+		o.newIkeSpiR,
+		o.conn.RemoteAddr())
+	// save Data
+	o.initRb = m.Data
+	o.fsm.PostEvent(state.IkeEvent{Id: state.CREATE_CHILD_SA_SUCCESS})
+}
